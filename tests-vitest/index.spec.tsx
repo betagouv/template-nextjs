49c5996076f2b207440835070ede25bb59dc9dd7
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-04-02 20:50:21 +0200

    vitest

--- tests-vitest/index.spec.tsx
+++ tests-vitest/index.spec.tsx
@@ -1,9 +1,9 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Page from "./index";
+import Page from "../src/pages/index";
 import React from "react";
 
-test("Page", () => {
+test("Has index page", () => {
   render(<Page />);
   expect(
     screen.getByRole("heading", { level: 1, name: "Template" })
