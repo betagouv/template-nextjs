a201762bfca84698a151372a810806a59fe378e9
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-05-30 23:36:31 +0200

    Update csp.config.mjs

--- csp.config.mjs
+++ csp.config.mjs
@@ -4,6 +4,7 @@ const ContentSecurityPolicy = `
   script-src 'self' *.gouv.fr ${
     process.env.NODE_ENV !== "production" && "'unsafe-eval' 'unsafe-inline'"
   };
+  connect-src 'self' *.gouv.fr https://sentry.incubateur.net;
   frame-src 'self' *.gouv.fr;
   style-src 'self' 'unsafe-inline';
   font-src 'self' data: blob:;
